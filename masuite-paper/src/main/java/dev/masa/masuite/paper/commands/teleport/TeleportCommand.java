8d9af84a5c103bf0eb61f63912473426c0033727
Matias Paavilainen <matias@paavilainen.net>
2022-01-12 20:53:07 +0200

    Added location teleportation

--- masuite-paper/src/main/java/dev/masa/masuite/paper/commands/teleport/TeleportCommand.java
+++ masuite-paper/src/main/java/dev/masa/masuite/paper/commands/teleport/TeleportCommand.java
@@ -2,6 +2,7 @@ package dev.masa.masuite.paper.commands.teleport;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
+import dev.masa.masuite.common.objects.Location;
 import dev.masa.masuite.common.objects.MaSuiteMessage;
 import dev.masa.masuite.paper.utils.BukkitPluginMessage;
 import org.bukkit.entity.Player;
@@ -22,6 +23,32 @@ public class TeleportCommand extends BaseCommand {
     @Description("Teleport player to player")
     public void teleportPlayerToPlayer(Player player, @Single String target, @Single String secondPlayer) {
         BukkitPluginMessage bpm = new BukkitPluginMessage(player, MaSuiteMessage.TELEPORT_PLAYER_TO_PLAYER, target, secondPlayer);
+        bpm.send();
+    }
+
+    @Default
+    @CommandPermission("masuite.teleport.location")
+    @Description("Teleport to location")
+    @CommandCompletion("@worlds")
+    public void teleportToLocation(Player player, @Single String world, double x, double y, double z) {
+        BukkitPluginMessage bpm = new BukkitPluginMessage(player, MaSuiteMessage.TELEPORT_TO_LOCATION, new Location(x, y, z, player.getLocation().getYaw(), player.getLocation().getPitch(), world).serialize());
+        bpm.send();
+    }
+
+    @Default
+    @CommandPermission("masuite.teleport.location")
+    @Description("Teleport to location")
+    public void teleportToLocation(Player player, double x, double y, double z) {
+        BukkitPluginMessage bpm = new BukkitPluginMessage(player, MaSuiteMessage.TELEPORT_TO_LOCATION, new Location(x, y, z, player.getLocation().getYaw(), player.getLocation().getPitch(), player.getLocation().getWorld().getName()).serialize());
+        bpm.send();
+    }
+
+    @Default
+    @CommandPermission("masuite.teleport.location.others")
+    @Description("Teleport to location")
+    @CommandCompletion("@masuite_players @worlds")
+    public void teleportPlayerToLocation(Player player, @Single String target, @Single String world, double x, double y, double z) {
+        BukkitPluginMessage bpm = new BukkitPluginMessage(player, MaSuiteMessage.TELEPORT_PLAYER_TO_LOCATION, target, new Location(x, y, z, player.getLocation().getYaw(), player.getLocation().getPitch(), world).serialize());
         bpm.send();
     }
 
--- masuite-velocity/src/main/java/dev/masa/masuite/velocity/listeners/teleport/TeleportMessageListener.java
+++ masuite-velocity/src/main/java/dev/masa/masuite/velocity/listeners/teleport/TeleportMessageListener.java
@@ -3,6 +3,8 @@ package dev.masa.masuite.velocity.listeners.teleport;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.proxy.Player;
+import dev.masa.masuite.api.proxy.listeners.teleport.ITeleportMessageListener;
+import dev.masa.masuite.common.objects.Location;
 import dev.masa.masuite.common.objects.MaSuiteMessage;
 import dev.masa.masuite.velocity.MaSuiteVelocity;
 
@@ -13,7 +15,7 @@ import java.util.Optional;
 
 import static dev.masa.masuite.velocity.MaSuiteVelocity.MASUITE_MAIN_CHANNEL;
 
-public record TeleportMessageListener(MaSuiteVelocity plugin) {
+public record TeleportMessageListener(MaSuiteVelocity plugin) implements ITeleportMessageListener<PluginMessageEvent> {
 
     @Subscribe
     public void teleportToPlayer(PluginMessageEvent event) throws IOException {
@@ -36,7 +38,7 @@ public record TeleportMessageListener(MaSuiteVelocity plugin) {
 
         this.plugin.teleportationService().teleportPlayerToPlayer(player, target.get(), (done) -> {
             // TODO: Send message to player
-            this.plugin.logger.info(player.getUsername() + " teleport to " + target.get().getUsername());
+            this.plugin.logger.info(player.getUsername() + " teleported to " + target.get().getUsername());
         });
     }
 
@@ -68,7 +70,56 @@ public record TeleportMessageListener(MaSuiteVelocity plugin) {
 
         this.plugin.teleportationService().teleportPlayerToPlayer(target.get(), secondTarget.get(), (done) -> {
             // TODO: Send message to executor
-            this.plugin.logger.info(player.getUsername() + " teleport to " + target.get().getUsername());
+            this.plugin.logger.info(player.getUsername() + " teleported to " + target.get().getUsername());
         });
     }
+
+    @Subscribe
+    public void teleportToLocation(PluginMessageEvent event) throws IOException {
+        if (!event.getIdentifier().equals(MASUITE_MAIN_CHANNEL)) return;
+
+        DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));
+        String channel = in.readUTF();
+        if (!channel.equals(MaSuiteMessage.TELEPORT_TO_LOCATION.channel)) {
+            return;
+        }
+        Player player = (Player) event.getTarget();
+
+        Location location = new Location().deserialize(in.readUTF());
+
+        this.plugin.teleportationService().teleportPlayerToLocation(player, location, (done) -> {
+            // TODO: Send message to player
+            this.plugin.logger.info(player.getUsername() + " teleported to " + location.serialize());
+        });
+    }
+
+    @Subscribe
+    public void teleportPlayerToLocation(PluginMessageEvent event) throws IOException {
+        if (!event.getIdentifier().equals(MASUITE_MAIN_CHANNEL)) return;
+
+        DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));
+        String channel = in.readUTF();
+        if (!channel.equals(MaSuiteMessage.TELEPORT_PLAYER_TO_LOCATION.channel)) {
+            return;
+        }
+        Player player = (Player) event.getTarget();
+
+        String name = in.readUTF();
+        Optional<Player> target = this.plugin.proxy().getPlayer(name);
+
+        if (target.isEmpty()) {
+            player.sendMessage(this.plugin.messages().playerNotOnline());
+            return;
+        }
+
+        Location location = new Location().deserialize(in.readUTF());
+        location.server(player.getCurrentServer().get().getServerInfo().getName());
+
+        this.plugin.teleportationService().teleportPlayerToLocation(target.get(), location, (done) -> {
+            // TODO: Send message to executor
+            this.plugin.logger.info(player.getUsername() + " teleported to " + location.serialize());
+        });
+    }
+
+
 }
